324ff80711b41fa58e88982e4bbc37bccf9e1b38
03Hari-Krishna-Kumar-div <hariiphones83@gmail.com>
2026-07-21 21:36:58 +0530

    Fix: remove set -e in entrypoint so container starts even if migrations fail

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -e
 
-php artisan migrate --force
-php artisan storage:link
+php artisan migrate --force 2>&1 || echo "Migration failed, continuing..."
+php artisan storage:link 2>&1 || echo "Storage link failed, continuing..."
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
