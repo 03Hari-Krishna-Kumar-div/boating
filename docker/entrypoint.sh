544472665451185cfa7fe72875d1a08b2e7feef8
03Hari-Krishna-Kumar-div <hariiphones83@gmail.com>
2026-07-21 21:50:27 +0530

    Fix: start supervisord first, then run migrations in background

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+/usr/bin/supervisord -c /etc/supervisord.conf &
+
+sleep 3
+
 php artisan migrate --force 2>&1 || echo "Migration failed, continuing..."
 php artisan storage:link 2>&1 || echo "Storage link failed, continuing..."
 
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+wait
